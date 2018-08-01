3c8c576e3fcbaf8e093e9b1ebf6b5d71ab698e68
Eric Weitz <eweitz@broadinstitute.org>
2018-08-01 14:26:05 -0400

    Implement igv.js in React using ES6 import

--- src/App.js
+++ src/App.js
@@ -2,20 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// The line below is enabled by ES6 export in igv.js.
+import igv from 'igv';
+
+var igvStyle = {
+  paddingTop: '10px',
+  paddingBottom: '10px',
+  margin: '8px',
+  border: '1px solid lightgray'
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to igv.js in React!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AppIgv/>
       </div>
     );
   }
 }
+
+class AppIgv extends Component {
+
+  componentDidMount() {
+    var igvContainer = document.getElementById('igv-div');
+    var igvOptions = {genome: 'hg38', locus: 'BRCA1'};
+    return igv.createBrowser(igvContainer, igvOptions);;
+  }
+
+  render() {
+    return (
+      <div id="igv-div" style={igvStyle}></div>
+    );
+  }
+}
+
 
 export default App;
