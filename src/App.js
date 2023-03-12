1ebd19ff7d7c3e1d9e1a12aa849468ac2d28225a
jrobinso <933148+jrobinso@users.noreply.github.com>
2023-03-11 19:36:35 -0800

    use reference to identify igv container rather than hardcode

--- src/App.js
+++ src/App.js
@@ -28,20 +28,21 @@ class AppIgv extends Component {
 
   constructor(props) {
     super(props)
+    // Declare reference object for the dom node that will contain the igv.js component
     this.container = createRef(null)
   }
 
 
   componentDidMount() {
-    //var igvContainer = document.getElementById('igv-div');
-    var igvOptions = {genome: 'hg38', locus: 'BRCA1'};
-    //return igv.createBrowser(igvContainer, igvOptions);
+    // Create the igv.js component.
+    const igvOptions = {genome: 'hg38', locus: 'BRCA1'};
     return igv.createBrowser(this.container.current, igvOptions);
   }
 
   render() {
+    // Return the div that will contain igv.js
     return (
-      <div id="igv-div" ref={this.container}  style={igvStyle}></div>
+      <div ref={this.container}  style={igvStyle}></div>
     );
   }
 }
